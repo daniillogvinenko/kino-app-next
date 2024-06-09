3f8353829b21fd87b33d8e29282c2c93cbc21526
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2024-06-09 10:19:50 +0300

    vercel build fix 5

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -2,38 +2,42 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
 
 export async function GET(req: NextRequest) {
-    const movies = await prisma.movie.findMany({
-        include: {
-            director: true,
-            mainActors: true,
-        },
-    });
-
-    const userId = req.nextUrl.searchParams.get("userId");
-    if (userId) {
-        const user = await prisma.user.findUnique({
-            where: {
-                username: userId,
+    try {
+        const movies = await prisma.movie.findMany({
+            include: {
+                director: true,
+                mainActors: true,
             },
         });
 
-        const moviesOfUser = await prisma.movie.findMany({
-            where: {
-                favoredBy: {
-                    some: {
-                        username: userId,
+        const userId = req.nextUrl.searchParams.get("userId");
+        if (userId) {
+            const user = await prisma.user.findUnique({
+                where: {
+                    username: userId,
+                },
+            });
+
+            const moviesOfUser = await prisma.movie.findMany({
+                where: {
+                    favoredBy: {
+                        some: {
+                            username: userId,
+                        },
                     },
                 },
-            },
-        });
-        return NextResponse.json(moviesOfUser);
-    }
+            });
+            return NextResponse.json(moviesOfUser);
+        }
 
-    const searchValue = req.nextUrl.searchParams.get("search");
-    if (searchValue) {
-        const filteredMovies = movies.filter((m) => m.title.toLowerCase().includes(searchValue.toLowerCase()));
-        return NextResponse.json(filteredMovies);
-    }
+        const searchValue = req.nextUrl.searchParams.get("search");
+        if (searchValue) {
+            const filteredMovies = movies.filter((m) => m.title.toLowerCase().includes(searchValue.toLowerCase()));
+            return NextResponse.json(filteredMovies);
+        }
 
-    return NextResponse.json(movies);
+        return NextResponse.json(movies);
+    } catch (error) {
+        console.log(error);
+    }
 }
