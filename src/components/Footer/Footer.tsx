1949e42ddce2ecf07a5776b1fcbfb9af28cb4d5d
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2024-06-16 23:25:05 +0300

    footer design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,11 @@
 import cls from "./Footer.module.scss";
 
 export const Footer = () => {
-    return <footer className={cls.Footer}>©Daniil Logvinenko</footer>;
+    return (
+        <footer className={cls.Footer}>
+            <div className="container">
+                <div className={cls.wrapper}>©Daniil Logvinenko</div>
+            </div>
+        </footer>
+    );
 };
