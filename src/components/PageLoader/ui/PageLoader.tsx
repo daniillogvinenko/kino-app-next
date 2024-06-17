7bceb267a0d6efa53d9da4e9a947a03b7ce53554
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2024-06-17 13:45:16 +0300

    delete review animation

--- src/components/PageLoader/ui/PageLoader.tsx
+++ src/components/PageLoader/ui/PageLoader.tsx
@@ -1,7 +1,20 @@
 import { API } from "@/shared/consts/consts";
 import Image from "next/image";
 import cls from "./PageLoader.module.scss";
+import { cn } from "@/shared/helpers/classNames/classNames";
 
-export const PageLoader = () => {
-    return <Image className={cls.PageLoader} src={`${API}/static/icons/appLogo.svg`} alt="" width={50} height={50} />;
+interface PageLoaderProps {
+    className?: string;
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => {
+    return (
+        <Image
+            className={cn(cls.PageLoader, {}, [className])}
+            src={`${API}/static/icons/appLogo.svg`}
+            alt=""
+            width={50}
+            height={50}
+        />
+    );
 };
--- src/components/SendReview/ui/SendReview.tsx
+++ src/components/SendReview/ui/SendReview.tsx
@@ -8,6 +8,9 @@ import axios from "axios";
 import { API } from "@/shared/consts/consts";
 import { useSession } from "next-auth/react";
 import { Review } from "@prisma/client";
+import Image from "next/image";
+import { cn } from "@/shared/helpers/classNames/classNames";
+import { PageLoader } from "@/components/PageLoader";
 
 interface SendReviewProps {
     movieId: string;
@@ -16,12 +19,11 @@ interface SendReviewProps {
 export const SendReview = ({ movieId }: SendReviewProps) => {
     const [value, setValue] = useState("");
     const [reviews, setReviews] = useState<Review[]>();
-    const [isLoading, setIsLoading] = useState(false);
+    const [commentIsDeleteing, setCommentIsDeleteing] = useState<string[]>([]);
     const session = useSession();
     const user = session.data?.user;
 
     const handleSendReview = () => {
-        setIsLoading(true);
         axios
             .post(`${API}/api/reviews`, {
                 text: value,
@@ -30,18 +32,19 @@ export const SendReview = ({ movieId }: SendReviewProps) => {
             })
             .then(() => {
                 setValue("");
-                axios
-                    .get(`${API}/api/reviews?movieId=${movieId}`)
-                    .then((response) => {
-                        setReviews(response.data);
-                    })
-                    .finally(() => setIsLoading(false));
+                axios.get(`${API}/api/reviews?movieId=${movieId}`).then((response) => {
+                    setReviews(response.data);
+                });
             })
             .catch((e) => console.log(e));
     };
 
     const handleDeleteReview = (reviewId: string) => {
-        setIsLoading(true);
+        setCommentIsDeleteing((p) => {
+            const originalArray = p;
+            originalArray.push(reviewId);
+            return JSON.parse(JSON.stringify(originalArray));
+        });
         axios
             .delete(`${API}/api/reviews`, {
                 data: {
@@ -49,12 +52,9 @@ export const SendReview = ({ movieId }: SendReviewProps) => {
                 },
             })
             .then(() => {
-                axios
-                    .get(`${API}/api/reviews?movieId=${movieId}`)
-                    .then((response) => {
-                        setReviews(response.data);
-                    })
-                    .finally(() => setIsLoading(false));
+                axios.get(`${API}/api/reviews?movieId=${movieId}`).then((response) => {
+                    setReviews(response.data);
+                });
             });
     };
 
@@ -85,30 +85,34 @@ export const SendReview = ({ movieId }: SendReviewProps) => {
                 </div>
             ) : null}
             <div className={cls.reviewsWrapper}>
-                {isLoading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <>
-                        {reviews?.map((r: any) => (
-                            <div className={cls.reviewCard} key={r.id}>
-                                <div className={cls.topFlex}>
-                                    <div className={cls.userAvatar}></div>
-                                    <p className={cls.username}>{r.user.username}</p>
-                                </div>
-                                <p className={cls.text}>{r.text}</p>
-                                <div className={cls.bottomFlex}>
-                                    <p className={cls.dateTime}>{new Date(r.dateTime).toDateString()}</p>
-
-                                    {user?.name === r.username ? (
-                                        <p onClick={() => handleDeleteReview(r.id)} className={cls.dateTime}>
-                                            Удалить комментарий
-                                        </p>
-                                    ) : null}
+                {reviews?.map((r: any) => (
+                    <div
+                        className={cn(cls.reviewCard, {}, [commentIsDeleteing.includes(r.id) ? cls.isLoading : ""])}
+                        key={r.id}
+                    >
+                        <PageLoader className={cls.cardLoader} />
+                        <div className={cls.topFlex}>
+                            <div className={cls.userAvatar}></div>
+                            <p className={cls.username}>{r.user.username}</p>
+                        </div>
+                        <p className={cls.text}>{r.text}</p>
+                        <div className={cls.bottomFlex}>
+                            <div className={cls.flexRight}>
+                                <p className={cls.dateTime}>{new Date(r.dateTime).toDateString()}</p>
+                                <div className={cls.reactionsWrapper}>
+                                    <Image src={`${API}/static/icons/like.svg`} alt="" height={18} width={18} />
+                                    <p>12</p>
+                                    <Image src={`${API}/static/icons/dislike.svg`} alt="" height={18} width={18} />
                                 </div>
                             </div>
-                        ))}
-                    </>
-                )}
+                            {user?.name === r.username ? (
+                                <p onClick={() => handleDeleteReview(r.id)} className={cls.dateTime}>
+                                    Удалить комментарий
+                                </p>
+                            ) : null}
+                        </div>
+                    </div>
+                ))}
             </div>
         </>
     );
