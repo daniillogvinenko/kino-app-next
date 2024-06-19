0ec4049259fc3a12a385f2093a6628f4f7bcb87d
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2024-06-19 14:14:59 +0300

    main page slider

--- src/components/MainPageHeroSection/ui/MainPageHeroSection.tsx
+++ src/components/MainPageHeroSection/ui/MainPageHeroSection.tsx
@@ -1,17 +1,49 @@
+"use client";
+
 import { Button } from "@/components/ui/Button";
 import cls from "./MainPageHeroSection.module.scss";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 export const MainPageHeroSection = () => {
+    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 2500 })]);
+
+    const items: { background: string; title: string; description: string }[] = [
+        {
+            background: 'url("/static/images/pages/hero-main-bg.png") center/cover fixed',
+            title: "Однажды в… Голливуде",
+            description: "2019 | Драма, комедия",
+        },
+        {
+            background: 'url("/static/images/pages/hero-main-bg2.jpg") center/cover fixed',
+            title: "Омерзительная восьмерка",
+            description: "2015 | Вестерн, криминал, триллер, драма, детектив",
+        },
+        {
+            background: 'url("/static/images/pages/hero-main-bg3.jpg") center/cover fixed',
+            title: "Крестный отец",
+            description: "1972 | Драма, криминал",
+        },
+    ];
+
     return (
-        <div className={cls.MainPageHeroSection}>
-            <div className="container">
-                <div className={cls.wrapper}>
-                    <h1>Однажды в… Голливуде</h1>
-                    <div className={cls.about}>2019 | Драма, комедия</div>
-                    <div className={cls.button}>
-                        <Button href="/movies/4">Смотреть</Button>
+        <div className="embla" ref={emblaRef}>
+            <div className="embla__container">
+                {items.map((item, index) => (
+                    <div key={index} className="embla__slide">
+                        <div style={{ background: item.background }} className={cls.MainPageHeroSection}>
+                            <div className="container">
+                                <div className={cls.wrapper}>
+                                    <h1>{item.title}</h1>
+                                    <div className={cls.about}>{item.description}</div>
+                                    <div className={cls.button}>
+                                        <Button href="/movies/4">Смотреть</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     );
