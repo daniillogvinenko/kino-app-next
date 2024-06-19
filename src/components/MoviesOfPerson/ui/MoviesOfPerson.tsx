ddcda9009d661c7b0ca27b383c7c8e5c47b3577c
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2024-06-19 16:04:52 +0300

    Movies of person switcher logic

--- src/components/MoviesOfPerson/ui/MoviesOfPerson.tsx
+++ src/components/MoviesOfPerson/ui/MoviesOfPerson.tsx
@@ -11,7 +11,7 @@ interface MoviesOfPersonProps {
 }
 
 export const MoviesOfPerson = ({ person, className }: MoviesOfPersonProps) => {
-    const [category, setCategory] = useState<"directed" | "acted">("acted");
+    const [category, setCategory] = useState<"directed" | "acted">(person.actedInMovies.length ? "acted" : "directed");
 
     const handleSetCategoryDirected = () => {
         setCategory("directed");
@@ -25,22 +25,26 @@ export const MoviesOfPerson = ({ person, className }: MoviesOfPersonProps) => {
         <div data-testid="MoviesOfPerson" className={cn(cls.MoviesOfPerson, {}, [className])}>
             <div className={cls.filmsTitle}>В главных ролях</div>
             <div className={cls.swithcerWrapper}>
-                <div data-testid="actedSwitcher" onClick={handleSetCategoryActed} className={cls.switcher}>
-                    <div className={cls.switcherCategory}>Актер</div>
-                    <div className={cls.switcherAmount}>{person?.actedInMovies.length} фильмов</div>
-                    <div
-                        style={{ display: category === "acted" ? "block" : "none" }}
-                        className={cls.switcherIndicator}
-                    ></div>
-                </div>
-                <div data-testid="directedSwitcher" onClick={handleSetCategoryDirected} className={cls.switcher}>
-                    <div className={cls.switcherCategory}>Режисер</div>
-                    <div className={cls.switcherAmount}>{person?.directedMovies.length} фильмов</div>
-                    <div
-                        style={{ display: category === "directed" ? "block" : "none" }}
-                        className={cls.switcherIndicator}
-                    ></div>
-                </div>
+                {person.actedInMovies.length ? (
+                    <div data-testid="actedSwitcher" onClick={handleSetCategoryActed} className={cls.switcher}>
+                        <div className={cls.switcherCategory}>Актер</div>
+                        <div className={cls.switcherAmount}>{person?.actedInMovies.length} фильмов</div>
+                        <div
+                            style={{ display: category === "acted" ? "block" : "none" }}
+                            className={cls.switcherIndicator}
+                        ></div>
+                    </div>
+                ) : null}
+                {person.directedMovies.length ? (
+                    <div data-testid="directedSwitcher" onClick={handleSetCategoryDirected} className={cls.switcher}>
+                        <div className={cls.switcherCategory}>Режисер</div>
+                        <div className={cls.switcherAmount}>{person?.directedMovies.length} фильмов</div>
+                        <div
+                            style={{ display: category === "directed" ? "block" : "none" }}
+                            className={cls.switcherIndicator}
+                        ></div>
+                    </div>
+                ) : null}
             </div>
             <div className={cls.separator}></div>
             {category === "acted" ? (
