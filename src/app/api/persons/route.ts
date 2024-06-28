d5dd95b1a5241f89e3be8476bdb2b5b37f0ea85c
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2024-06-28 20:37:45 +0300

    search movies and persons

--- src/app/api/persons/route.ts
+++ src/app/api/persons/route.ts
@@ -1,13 +1,19 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
     const persons = await prisma.person.findMany({
         include: {
             actedInMovies: true,
             directedMovies: true,
         },
     });
+
+    const searchValue = req.nextUrl.searchParams.get("search");
+    if (searchValue) {
+        const filteredPersons = persons.filter((p) => p.fullName.toLowerCase().includes(searchValue.toLowerCase()));
+        return NextResponse.json(filteredPersons);
+    }
 
     return NextResponse.json(persons);
 }
--- src/components/Header/ui/SearchBar/SearchBar.tsx
+++ src/components/Header/ui/SearchBar/SearchBar.tsx
@@ -6,31 +6,36 @@ import Input from "@/components/ui/Input";
 import { Overlay } from "@/components/ui/Overlay";
 import { API } from "@/shared/consts/consts";
 import { PageLoader } from "@/components/ui/PageLoader";
+import { Movie, Person } from "@prisma/client";
+import { Button } from "@/components/ui/Button";
 
 export const SearchBar = () => {
     const [isSearchOpened, setIsSearchOpened] = useState<boolean>(false);
     const [searchValue, setSearchValue] = useState("");
-    const [searchResultItems, setSearchResultItems] = useState<any[]>([]);
+    const [searchResultMovies, setSearchResultMovies] = useState<Movie[]>([]);
+    const [searchResultPersons, setSearchResultPersons] = useState<Person[]>([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false);
     const inputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         if (searchValue) {
-            const fetchData = async () => {
-                const data = await fetch(`${API}/api/movies?search=${searchValue}`, { cache: "no-store" })
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .catch(() => {
-                        setError(true);
-                    });
-                return data;
-            };
-
             setIsLoading(true);
-            fetchData().then((data) => {
-                setSearchResultItems(data);
+            // fetch(`${API}/api/movies?search=${searchValue}`, { cache: "no-store" })
+            //     .then((response) => response.json())
+            //     .then((data) => {
+            //         setSearchResultMovies(data);
+            //         setIsLoading(false);
+            //     })
+            //     .catch(() => setError(true));
+
+            const p1 = fetch(`${API}/api/movies?search=${searchValue}`, { cache: "no-store" });
+            const p2 = fetch(`${API}/api/persons?search=${searchValue}`, { cache: "no-store" });
+            Promise.all([p1, p2]).then(async ([res1, res2]) => {
+                const movies: Movie[] = await res1.json();
+                const persons: Person[] = await res2.json();
+                setSearchResultMovies(movies);
+                setSearchResultPersons(persons);
                 setIsLoading(false);
             });
         }
@@ -86,10 +91,11 @@ export const SearchBar = () => {
                         {error ? (
                             <div>Произошла ошибка при загрузке</div>
                         ) : (
-                            <>
-                                {searchResultItems.length ? (
-                                    <ul className={cls.searchResults}>
-                                        {searchResultItems?.map((movie) => (
+                            <div className={cls.searchResults}>
+                                {searchResultMovies.length ? (
+                                    <ul>
+                                        <p className={cls.category}>Фильмы</p>
+                                        {searchResultMovies?.slice(0, 3).map((movie) => (
                                             <Link
                                                 onClick={handleCloseSearch}
                                                 key={movie.id}
@@ -105,7 +111,8 @@ export const SearchBar = () => {
                                                     <div className={cls.movieInfoWrapper}>
                                                         <p>{movie.title}</p>
                                                         <p className={cls.movieInfo}>
-                                                            <span>8,5</span>Lorem, ipsum dolor
+                                                            <span>{movie.rating!.toString().split("").join(",")}</span>
+                                                            {movie.country}
                                                         </p>
                                                     </div>
                                                 </li>
@@ -113,7 +120,33 @@ export const SearchBar = () => {
                                         ))}
                                     </ul>
                                 ) : null}
-                            </>
+                                {searchResultPersons.length ? (
+                                    <ul>
+                                        <p className={cls.category}>Персоны</p>
+                                        {searchResultPersons?.slice(0, 3).map((person) => (
+                                            <Link
+                                                onClick={handleCloseSearch}
+                                                key={person.id}
+                                                href={`/movies/${person.id}`}
+                                            >
+                                                <li>
+                                                    <Image
+                                                        src={`/static/images/persons/${person.mainImage}`}
+                                                        alt=""
+                                                        width={43}
+                                                        height={65}
+                                                    />
+                                                    <div className={cls.movieInfoWrapper}>
+                                                        <p>{person.fullName}</p>
+                                                        <p className={cls.movieInfo}>{person.fullNameEnglish}</p>
+                                                    </div>
+                                                </li>
+                                            </Link>
+                                        ))}
+                                    </ul>
+                                ) : null}
+                                <Button variant={"borderless"}>Показать все результаты</Button>
+                            </div>
                         )}
                     </>
                 )}
